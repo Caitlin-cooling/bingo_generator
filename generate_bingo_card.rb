ea2bf8a11446aa03fe2c735907a1b6e0233eb461
Caitlin Cooling <ccooling@deloitte.co.uk>
2019-09-18 14:44:27 +0100

    Ensures that different titles are used for csvs to prevent overriding them

--- generate_bingo_card.rb
+++ generate_bingo_card.rb
@@ -3,18 +3,25 @@ require 'csv'
 class Generator
   def initialize(array)
     @array = array
+    @counter = 0
   end
 
-  def randomise
+  def randomised_array
     @array.shuffle
   end
 
+  def create_title
+    @counter += 1
+    "words_#{@counter}.csv" 
+  end
+
   def generate_csv
-    CSV.open('words.csv', 'w') do |csv_obj|
-      csv_obj << randomise
+    CSV.open(create_title, 'w') do |csv_obj|
+      csv_obj << randomised_array
     end
   end
 end
 
 generator = Generator.new([1, 2, 3, 4, 5])
+generator.generate_csv
 generator.generate_csv
